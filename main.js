af69759c031b7d4cb3dd24d6d77c37cb6297e87d
28akusetti-docter <83176122+28akusetti-docter@users.noreply.github.com>
2022-01-29 10:18:41 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 550);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet is Initialized!');
+}
+
+function draw() {
+    background('#87CEEB');
+    document.getElementById("square_side").innerHTML = "Font Size of the text will be =" + difference +"px";
+    textSize(difference)
+    fill('#f6df43');
+    text("Cutie", noseX, noseY)
+}
+
+function gotPoses(results) {
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX +" noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX =" + leftWristX +" rightWristX "+ rightWristX + "difference =" + difference);
+    }
+
+}
